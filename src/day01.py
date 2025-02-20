cf06d26cd3b0fa472ee36f7f71da9c0ee2a0d94c
Luke Vass <luke.jack.vass@gmail.com>
2025-02-20 17:25:55 +0000

    day1 done!

--- src/day01.py
+++ src/day01.py
@@ -15,6 +15,24 @@ def solve_part_1(filename):
     modules = fetch_data(filename)
     return total_fuel(modules)
 
+def fuel_required_p2(mass):
+    total_so_far = 0
+    fuel = fuel_required(mass)
+    while fuel > 0:
+        total_so_far += fuel
+        fuel = fuel_required(fuel)
+    return total_so_far
+
+def solve_part_2(filename):
+    modules = fetch_data(filename)
+    return total_fuel_p2(modules)
+
+def total_fuel_p2(modules):
+    requirements = (fuel_required_p2(m) for m in modules)
+    return sum(requirements)
+ 
+
+
 
 # ---- tests ---- #
 
@@ -26,4 +44,15 @@ def test_total_fuel():
     assert total_fuel([12, 1969]) == 656
 
 def test_solve_part_1():
-    assert solve_part_1("input/day01.input.txt") == 656
+    assert solve_part_1("input/day01.test.input.txt") == 656
+
+def test_fuel_required_p2() :
+    assert fuel_required_p2(14) == 2
+    assert fuel_required_p2(1969) == 966
+    assert fuel_required_p2(100756) == 50346
+
+def test_total_fuel_p2():
+    assert total_fuel_p2([100756, 1969]) == (50346 + 966)
+
+if __name__ == "__main__":
+    print(solve_part_2("input/day01.input.txt"))
